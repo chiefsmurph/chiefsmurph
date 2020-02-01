44eadf72163e047b4a05ec4c51dc9da9122b3c79
chiefsmurph <chiefsmurph@gmail.com>
2020-02-01 11:56:03 -0800

    projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AutoPlayAudio from './AutoPlayAudio';
+import projects from './projects';
 
 import logo from './logo.svg';
 import './App.css';
@@ -20,10 +21,38 @@ const App: React.FC = () => {
         </div>
         <h1>chiefsmurph.com</h1>
       </header>
-      <ul>
-        <li><a href="/stocks" target="blank">Stocks</a></li>
-        <li><a href="/circlebattle" target="blank">CircleBattle</a></li>
-      </ul>
+      <main>
+        {
+          projects.map(({ section: sectionName, links }: any) => (
+            <section>
+              <h2>{sectionName}</h2>
+              <ul>
+                {
+                  links.map(({ name: projectName, url = '/#' }: any) => (
+                    <li><a href={url}>{projectName}</a></li>
+                  ))
+                }
+              </ul>
+            </section>
+          ))
+        }
+{/*         
+        <section>
+        <h2>Games</h2>
+        </section>
+        <ul>
+          <li><a href="/circlebattle" target="blank">CircleBattle</a></li>
+        </ul>
+        <h2>Not Games</h2>
+        <ul>
+          <li><a href="/stocks" target="blank">Stocks</a></li>
+        </ul>
+        <h2>Contributed To</h2>
+        <ul>
+          <li><a href="/#" target="blank">Scrapin' It</a></li>
+          <li><a href="/#" target="blank">Foodhyped</a></li>
+        </ul> */}
+      </main>
       <AutoPlayAudio/>
     </div>
   );
---
+++ src/projects.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    section: 'Games',
+    links: [{
+      name: 'CircleBattle',
+      url: '/circlebattle'
+    }]
+  },
+  {
+    section: 'Not Games',
+    links: [{
+      name: 'Stocks',
+      url: '/stocks'
+    }],
+  },
+  {
+    section: 'Contributed To',
+    links: [
+      {
+        name: `Scrapin 'It`,
+      },
+      {
+        name: 'Foodhyped',
+      },
+  ]
+  },
+];
