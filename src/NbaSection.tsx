f8d54fd8b5a1fbb548797123cb5ec2ccf8b6a7bf
chiefsmurph <chiefsmurph@gmail.com>
2021-01-17 17:36:59 -0600

    lets go

--- src/NbaSection.tsx
+++ src/NbaSection.tsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
 
 export default () => {
+    const [date, setDate] = useState(new Date());
     const [predictions, setPredictions] = useState({} as any);
     useEffect(() => {
-        fetch('/pattern-predict/nba?json').then(response => response.json()).then(setPredictions);
-    }, []);
+        fetch(`/pattern-predict/nba/${date}?json`).then(response => response.json()).then(setPredictions);
+    }, [date]);
     return (
         <section>
             <h2>Nba Predictions</h2>
             {
                 predictions.predictions ? (
-                    <div>
-                        <i>for {predictions.date}...</i>
+                    <div style={{ zoom: '70%' }}>
+                        <DatePicker selected={date} onChange={date => setDate(date)} />
                         <ul>
                             {
                                 predictions.predictions.map(line => <li>{line}</li>)
