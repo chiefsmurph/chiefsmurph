d40853db17b587329cf19d3bc7fb84ed773f1fe2
chiefsmurph <chiefsmurph@gmail.com>
2021-01-21 21:35:08 -0600

    ok

--- src/NbaSection.tsx
+++ src/NbaSection.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
+import Modal from 'react-modal';
 require('react-datepicker/dist/react-datepicker.css')
 
 export default () => {
     const [date, setDate] = useState(new Date());
     const [loading, setLoading] = useState(false);
     const [predictions, setPredictions] = useState({} as any);
+    const [modalShowing,setModalShowing] = React.useState(null);
     useEffect(() => {
         setLoading(true);
         fetch(`/pattern-predict/nba/${date}?json`).then(response => response.json()).then(data => {
@@ -14,6 +16,9 @@ export default () => {
         });
     }, [date]);
     const alertText = game => JSON.stringify(game, null, 2);
+    function closeModal(){
+      setModalShowing(null);
+    }
     return (
         <section>
             <h2>Nba Predictions</h2>
@@ -25,8 +30,8 @@ export default () => {
                             {
                                 predictions.games.map(({ teams: { home, away }, prediction }) => (
                                     <li>
-                                        <a href='javascript:void(0)' onClick={evt => { alert(alertText(away)); evt.preventDefault(); }}>{away.name} ({away.record})</a> @&nbsp;
-                                        <a href='javascript:void(0)' onClick={evt => { alert(alertText(home)); evt.preventDefault(); }}>{home.name} ({home.record})</a>&nbsp;
+                                        <a href='javascript:void(0)' onClick={evt => { setModalShowing(away); evt.preventDefault(); }}>{away.name} ({away.record})</a> @&nbsp;
+                                        <a href='javascript:void(0)' onClick={evt => { setModalShowing(home); evt.preventDefault(); }}>{home.name} ({home.record})</a>&nbsp;
                                         predicted winner: {prediction.winningTeam} with {Math.round(prediction.confidence)} confidence
                                     </li>
                                 ))
@@ -35,6 +40,19 @@ export default () => {
                     </div>
                 ) : <small>loading</small>
             }
+            <Modal
+                isOpen={Boolean(modalShowing)}
+                onRequestClose={closeModal}
+                contentLabel="NBA Details"
+            >
+                <button onClick={closeModal}>Close Modal</button>
+                <pre>
+                    {
+                        JSON.stringify(modalShowing, null, 2)
+                    }
+                </pre>
+
+            </Modal>
         </section>
     )
 }
